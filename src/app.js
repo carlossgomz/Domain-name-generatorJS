f11ec5e8fb106311e96b764141c22b2bf8516cd8
Carlos Gómez <carlosgomz1331@gmail.com>
2022-08-19 12:54:26 -0400

    finished Domain name Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let domainGenerator = [];
+  var pronoun = ['the','our'];
+  var adj = ['great', 'big' ];
+  var noun = ['jogger','racoon'];
+  let ext = [".com", ".net", ".us", ".io"];
+
+  for (let a = 0; a < pronoun.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        for (let d = 0; d < ext.length; d++) {
+          domainGenerator.push("  " + pronoun[a] + adj[b] + noun[c] + ext[d]);
+        }
+      }
+    }
+  }
+  console.log(domainGenerator);
+  document.getElementById("domain.results").innerHTML = domainGenerator;
 };
